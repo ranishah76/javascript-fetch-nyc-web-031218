4bd6fa8024510cfdf686c53a95d584388c12b88c
Rani Shah <rani.shah@flatironschool.com>
2018-04-29 21:14:19 -0400

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
 const app = "I don't do much.";
+
+const token = 'd9bc57ac0b87ffa49ee5eef3ba3c95663a13d362'
+fetch('https://api.github.com/user/repos', {
+  headers: {
+    Authorization: `token ${token}`
+  }
+}).then(res => res.json()).then(json => console.log(json));
